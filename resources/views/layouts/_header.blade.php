22b11f5d3045c0c7aaf3ad329e9f308d991d190e
vagrant <vagrant@homestead>
2021-09-03 04:18:07 +0000

    命名路由

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,8 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container">
-    <a class="navbar-brand" href="/">run App</a>
+    <a class="navbar-brand" href={{route("home")}}>run App</a>
     <u1 class="navbar-nav justify-content-end">
-      <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+      <li class="nav-item"><a class="nav-link" href={{route("help")}}>帮助</a></li>
       <li class="nav-item"><a class="nav-link" herf="#">登录</a></li>
     </u1>
   </div>
