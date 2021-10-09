ddee9894145ea7f52da99eb32335d8e96ddc2738
vagrant <vagrant@homestead>
2021-10-09 02:23:32 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -73,7 +73,62 @@ public function gravatar($size = '100')
     {
         return $this->statuses()
                     ->orderBy('created_at','desc');
+
+    }
+
+    //粉丝关系列表
+    public function followers()
+    {
+        //belongsToMany(User:class,'自定义的表名称','定义在关联的模型外键名','要合并的模型外键名')
+        return $this->belongsToMany(User::class,'followers','user_id','follower_id');
     }
 
 
+    //用户关注列表
+    public function followings()
+    {
+        return $this->belongsToMany(User::class,'followers','follower_id','user_id');
+    }
+
+
+    public function follow($user_ids)
+    {
+        //判断user_id是否是一个数组，如果不是，就用compact方法转化为数组
+        if(! is_array(user_ids)){
+            $user_ids = compact('user_ids');
+        }
+        //用sync方法将对象加入关注列表
+        $this->followings()->sync($user_ids,false);
+    }
+
+
+    public function unfollow($user_ids)
+    {
+        if( ! is_array($user_ids)){
+            $user_ids = compact('user_ids');
+        }
+        //detach方法可以将用户从关注列表里删除
+        $this->followings()->detach($user_ids);
+    }
+
+//followings()和followings方法的区别
+
+    //解释1
+        // 1.返回的是一个HasMany对象
+        // $this->followings();
+        // 2.返回的是一个Collection集合
+        // $this->followings;
+        // 实质上等于$this->followings()->get();
+
+    // 解释2
+        // 因为 contains 方法是 Collection 类的一个方法，$this->followings 返回的是一个 Collection 类的实例，也就是一个集合，但是 $this->followings() 返回的是一个 Relations，没有 contains 方法，所以不能加括号。
+
+
+
+
+    //判断当前用户a是否已经关注该用户b,并返回判断
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
+    }
 }
